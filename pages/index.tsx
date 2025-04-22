8633eec0b122638a2a630870fcfcf8d9b07a9e27
qkas <kassuvaa@gmail.com>
2025-04-23 00:57:46 +0300

    Add font-bold to title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Counter from "@/components/Counter";
 export default function Home() {
   return (
     <div className="flex flex-col items-center space-y-4">
-      <h1 className="mt-20 text-5xl">Scores</h1>
+      <h1 className="mt-20 text-5xl font-bold">Scores</h1>
       <Counter name="Kasperi"/>
       <Counter name="Opponent"/>
     </div>
