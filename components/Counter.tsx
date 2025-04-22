551f9e1b690893bfcf0f32f98f9fa544f120088a
qkas <kassuvaa@gmail.com>
2025-04-22 18:34:32 +0300

    Break my code

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -16,7 +16,7 @@ export default function Counter({ name }: CounterProps) {
       <div className="flex gap-5">
         <button
           className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-          onClick={() => setCount(count + 1)}
+          onClick={() => setCount(count + 2)}
         >
           +
         </button>
