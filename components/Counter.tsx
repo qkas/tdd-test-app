bfccce9b8df169e124fc606742671f8156b48252
qkas <kassuvaa@gmail.com>
2025-04-23 00:14:09 +0300

    Change color of add button to blue

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -15,7 +15,7 @@ export default function Counter({ name }: CounterProps) {
       </div>
       <div className="flex gap-5">
         <button
-          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           onClick={() => setCount(count + 1)}
         >
           +
